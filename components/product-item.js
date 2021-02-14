a1486a981269149d79356b63ee7384548c2f2122
ibro912 <ibro@ucsd.edu>
2021-02-13 19:14:19 -0800

    product item element

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,36 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+	constructor()
+	{
+		super();
+
+		let shadow = this.attachShadow({mode: 'open'});
+
+		const product = document.createElement('li');
+		product.setAttribute('class', 'product');
+
+		const image = document.createElement('img');
+		
+		product.appendChild(image);
+
+		const title = document.createElement('p');
+		title.setAttribute('class', 'title');
+
+		product.appendChild(title);
+
+		const price = document.createElement('p');
+		price.setAttribute('class', 'price');
+
+		product.appendChild(price);
+
+		const linkElem = document.createElement('link');
+		linkElem.setAttribute('rel', 'stylesheet');
+		linkElem.setAttribute('href', 'styles/styles.css');
+
+		shadow.appendChild(product);
+		shadow.appendChild(linkElem);
+	}
 }
 
 customElements.define('product-item', ProductItem);
