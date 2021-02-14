73da58b4f7bfa9343fadea8e4ff031c9b6837a61
ibro912 <ibro@ucsd.edu>
2021-02-13 18:06:26 -0800

    updating script for part 1 and 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,17 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+	//fetch request
+  
+	let response = await fetch('https://fakestoreapi.com/products');
+
+	let products = await response.json();
+  	if(!localStorage.get(prods))
+  	{
+		window.localStorage.setItem(prods, products.stringify());
+	}
+    else
+    {
+        products = JSON.parse(window.localStorage.getItem('products'));
+    }
 });
